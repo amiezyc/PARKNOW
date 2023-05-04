dd91c1045fe99dd7c9b4328cc9916b0f8ef9d0d7
peter <109552816+petermvgz@users.noreply.github.com>
2023-05-03 23:44:50 -0700

    Initial Commit

---
+++ iParkApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iParkApp
+//
+//  Created by Peter Deyi on 5/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
