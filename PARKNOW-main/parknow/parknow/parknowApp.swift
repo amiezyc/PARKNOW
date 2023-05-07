b25b9e142a439c01ae30229881de0f1f58785e61
Peter Deyi <peterdeyi@gmail.com>
2023-05-06 19:40:13 -0700

    Added .ignoreSafeArea() to the MapView

---
+++ PARKNOW-main/parknow/parknow/parknowApp.swift
@@ -0,0 +1,17 @@
+//
+//  parknowApp.swift
+//  parknow
+//
+//  Created by 周雨橙 on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct parknowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
